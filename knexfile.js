e6cdc42dbc57584a61eb59ff25e817c3b6654fec
Oyefule <drelanorgent@gmail.com>
2025-03-06 11:13:13 +0100

    Updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,11 @@
 module.exports = {
    client: 'mysql2',
    connection: {
-      host: process.env.DBHost,
-      port: parseInt(process.env.DBPort),
-      user: process.env.DBUser,
-      password: process.env.DBPassword,
-      database: process.env.DB,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_STRING,
    },
    migrations: {
       directory: __dirname + "/src/internal/migrations",
