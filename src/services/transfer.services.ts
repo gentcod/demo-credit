b302ceb70ad07742aeb054d4edfe55b7ac053a47
Oyefule <drelanorgent@gmail.com>
2025-03-09 15:36:07 +0100

    Updated Transfer API for more error handling and abstraction

--- src/services/transfer.services.ts
+++ src/services/transfer.services.ts
@@ -2,11 +2,13 @@ import { AccountResponse } from "../dtos/account.dto";
 import { db } from "../db";
 import { TransferDto } from "../dtos/transfer.dto";
 import { ApiResponse } from "../utils/apiResponse";
+import { WalletIDGenerator } from "../utils/account";
 
 type ValidAccountRes = {
    isValid: boolean;
    sender_id: string;
    recipient_id: string;
+   type?: string;
 }
 
 export class TransferServices {
@@ -19,6 +21,13 @@ export class TransferServices {
          }
       }
 
+      const walletIsValid = new WalletIDGenerator().isValid(transfer.wallet_id)
+      if (!walletIsValid) {
+         return {
+            status: 400,
+            message: 'Transaction cannot be executed. Invalid wallet id.'
+         }
+      }
 
       const validRes = await this._validateAccoount(
          auth[0].id, transfer.wallet_id, transfer.amount, transfer.currency
@@ -27,12 +36,12 @@ export class TransferServices {
          const msg = validRes.recipient_id === undefined
             ? 'Recipient account does not exist'
             :
-         validRes.sender_id === 'same'
-            ? 'You cannot send money to yourself'
-            :
-         validRes.sender_id === 'low' 
-            ? 'Insufficient funds'
-            : `Wallet is not a ${transfer.currency} based wallet.`
+            validRes.type === 'same'
+               ? 'You cannot send money to yourself'
+               :
+               validRes.type === 'low'
+                  ? 'Insufficient funds'
+                  : `Wallet is not a ${transfer.currency} based wallet.`
 
          const code = validRes.recipient_id === undefined ? 404 : 400
          return {
@@ -41,13 +50,15 @@ export class TransferServices {
          }
       }
 
-      const account = await db.querier.transferTx.transfer({
+      await db.querier.transferTx.transfer({
          sender_id: validRes.sender_id,
          recipient_id: validRes.recipient_id,
          amount: transfer.amount
       }, transfer.currency);
 
-      const resp = AccountResponse.createResponse(account)
+      const updatedAccount = await db.querier.account.getAccount(auth[0].id, transfer.currency);      
+
+      const resp = AccountResponse.createResponse(updatedAccount[0])
 
       return {
          status: 200,
@@ -64,15 +75,15 @@ export class TransferServices {
       const sender = await db.querier.account.getAccount(senderId, currency);
       const diff = sender[0].balance - amount;
       if (sender[0].balance < amount || diff < 0) {
-         return { sender_id: 'low', recipient_id: '', isValid: false }
+         return { sender_id: '', recipient_id: '', isValid: false, type: 'low' }
       };
       const recipient = await db.querier.account.getAccountByWalletId(wallet_id);
       if (!recipient[0]) {
          return { sender_id: '', recipient_id: undefined, isValid: false }
       }
-            
+
       if (sender[0].id === recipient[0].id) {
-         return { sender_id: 'same', recipient_id: '', isValid: false }
+         return { sender_id: '', recipient_id: '', isValid: false, type: 'same' }
       }
 
       if (sender[0].currency !== recipient[0].currency) {
